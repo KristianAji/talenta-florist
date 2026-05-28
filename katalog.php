cfdfe435c703afed99cc8953e6103abc09a30ba6
Delon Poluakan <delonpoluakan2@gmail.com>
2026-05-28 19:19:41 +0800

    Update katalog.php

--- katalog.php
+++ katalog.php
@@ -130,9 +130,11 @@ $produk_list = array_values($produk_map);
     <a class="logo" href="index.php">Talenta <span>Florist</span></a>
     <ul class="nav-links">
       <li><a href="tentang.php">Tentang</a></li>
-      <li><a href="katalog.php" class="active">Katalog</a></li>
+      <li><a href="katalog.php">Katalog</a></li>
       <li><a href="pesan.php">Cara Pesan</a></li>
       <li><a href="kontak.php">Kontak</a></li>
+      <li><a href="register.php">Daftar</a></li>
+      <li><a href="login.php">Login</a></li>
     </ul>
     <a class="btn btn-primary" href="https://wa.me/6285233608339" target="_blank">💬 WhatsApp</a>
   </nav>
