dbf384ce5d59fa5a951a46bb178f8f83c9d81b73
Delon Poluakan <delonpoluakan2@gmail.com>
2026-05-28 19:19:16 +0800

    Update kontak.php

--- kontak.php
+++ kontak.php
@@ -76,7 +76,9 @@
       <li><a href="tentang.php">Tentang</a></li>
       <li><a href="katalog.php">Katalog</a></li>
       <li><a href="pesan.php">Cara Pesan</a></li>
-      <li><a href="kontak.php" class="active">Kontak</a></li>
+      <li><a href="kontak.php">Kontak</a></li>
+      <li><a href="register.php">Daftar</a></li>
+      <li><a href="login.php">Login</a></li>
     </ul>
     <a class="btn btn-primary" href="https://wa.me/6285233608339" target="_blank">💬 WhatsApp</a>
   </nav>
